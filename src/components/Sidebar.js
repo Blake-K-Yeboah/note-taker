596559a826adab6adbbdb468d39969ff850c65ca
Blake Yeboah <blakekyeboah@gmail.com>
2021-11-27 15:49:25 +1000

    Created Sidebar Component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = ({ setModalShow }) => {
+    return (
+        <div className="bg-gray-200 w-64 h-screen fixed left-0 top-0">
+            <h2 className="pl-5 text-gray-600 text-2xl py-10">Your Notes</h2>
+            <button
+                onClick={() => setModalShow(true)}
+                className="bg-green-500 hover:bg-green-700 transition-all duration-300 ease-in-out text-white px-5 py-3 rounded-xl ml-5 fixed bottom-10"
+            >
+                Create Note
+            </button>
+        </div>
+    );
+};
+
+export default Sidebar;
