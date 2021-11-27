e473aa13090d429adf23adb8dd20fa14087fb0dd
Blake Yeboah <blakekyeboah@gmail.com>
2021-11-27 16:41:49 +1000

    Changed z-index

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sidebar = ({ setModalShow }) => {
     return (
-        <div className="bg-gray-200 w-64 h-screen fixed left-0 top-0">
+        <div className="bg-gray-200 w-64 h-screen fixed left-0 top-0 z-10">
             <h2 className="pl-5 text-gray-600 text-2xl py-10">Your Notes</h2>
             <button
                 onClick={() => setModalShow(true)}
