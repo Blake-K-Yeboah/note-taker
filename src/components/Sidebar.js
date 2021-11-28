eda13f596c82c94375c76512676d1bd8fdb4fe57
Blake Yeboah <blakekyeboah@gmail.com>
2021-11-29 01:22:47 +1000

    Updated Colors

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,7 +10,7 @@ const Sidebar = ({
     setActiveNote,
 }) => {
     return (
-        <div className="bg-gray-700 w-64 h-screen fixed left-0 top-0 z-10">
+        <div className="bg-gray-700 w-64 h-screen fixed left-0 top-0 z-20">
             <h2 className="pl-5 text-gray-200 text-2xl py-10">Your Notes</h2>
             {notes.map((note) => {
                 return (
@@ -27,7 +27,7 @@ const Sidebar = ({
             })}
             <button
                 onClick={() => setModalShow(true)}
-                className="bg-green-500 hover:bg-green-700 transition-all duration-300 ease-in-out text-white px-5 py-3 rounded-xl ml-5 fixed bottom-10"
+                className="bg-blue-500 hover:bg-blue-700 transition-all duration-300 ease-in-out text-white px-5 py-3 rounded-xl ml-5 fixed bottom-10"
             >
                 Create Note
             </button>
