14583af71a43da10288e29d3c5d8d0ee8628e72f
Blake Yeboah <blakekyeboah@gmail.com>
2021-11-29 01:22:27 +1000

    Added Editable Title (not finished yet)

--- src/components/CurrentEditor.js
+++ src/components/CurrentEditor.js
@@ -1,3 +1,9 @@
+// Hooks
+import { useState, useRef } from "react";
+
+// Icons
+import { FaCheck } from "react-icons/fa";
+
 const CurrentEditor = ({
     notes,
     currentNote,
@@ -5,11 +11,41 @@ const CurrentEditor = ({
     activeNote,
     setActiveNote,
 }) => {
+    const [titleEditable, setTitleEditable] = useState(true);
+    const [title, setTitle] = useState("Class Notes");
+    const titleInputEl = useRef(null);
+
     return (
-        <div className="bg-gray-800 w-screen h-screen py-10 pl-64">
-            <h1 className="pl-16 text-5xl text-gray-300 font-light border-b-2 border-gray-600 pb-8">
-                {activeNote ? activeNote.title : "Loading..."}
-            </h1>
+        <div className="bg-gray-800 w-screen h-screen py-10 pl-64 z-10">
+            {!titleEditable ? (
+                <h1
+                    className=" pl-16 text-5xl text-gray-300 font-light border-b-2 border-gray-600 pb-8"
+                    onClick={() => {
+                        setTitle(activeNote.title);
+                        setTitleEditable(true);
+                        setTimeout(() => titleInputEl.current.focus(), 150);
+                    }}
+                >
+                    {activeNote ? activeNote.title : "Loading..."}
+                </h1>
+            ) : (
+                <div className="flex flex-row relative">
+                    <input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Enter Title"
+                        className="bg-gray-800 text-5xl text-gray-300 font-light border-b-2 border-blue-600 pb-4 pl-16 outline-none focus:shadow-2xl w-screen"
+                        ref={titleInputEl}
+                    />
+                    <button
+                        className="text-white bg-green-400 ml-5 rounded-xl mt-5 
+                        hover:bg-green-600 cursor-pointer transition-all duration-300 ease-in-out w-32 h-10 flex justify-center items-center absolute left-12 top-20"
+                        onClick={() => setTitleEditable(false)}
+                    >
+                        Save Title <FaCheck className="ml-3" />
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
