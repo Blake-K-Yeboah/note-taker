3d5e9a0aaa5dfae7f292afd6e5270adb2842d3cf
Blake Yeboah <blakekyeboah@gmail.com>
2021-11-28 21:54:17 +1000

    Fixed layout and added CurrentEditor component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 // Components
 import Sidebar from "./components/Sidebar";
+import CreateNote from "./components/CreateNote";
+import CurrentEditor from "./components/CurrentEditor";
 
 // Hooks
 import { useEffect, useState } from "react";
-import CreateNote from "./components/CreateNote";
 
 function App() {
     const [currentNote, setCurrentNote] = useState("");
     const [notes, setNotes] = useState([]);
     const [modalShow, setModalShow] = useState(false);
+
+    const [activeNote, setActiveNote] = useState({});
 
     useEffect(() => {
         let notesFromStorage = localStorage.getItem("notes");
@@ -16,18 +19,36 @@ function App() {
         if (notesFromStorage) {
             setCurrentNote(JSON.parse(notesFromStorage)[0].title);
             setNotes(JSON.parse(notesFromStorage));
+            setActiveNote(JSON.parse(notesFromStorage)[0]);
         }
     }, []);
 
     return (
-        <div className="flex flex-row">
-            <Sidebar
-                currentNote={currentNote}
-                notes={notes}
+        <>
+            <div className="flex flex-row">
+                <Sidebar
+                    currentNote={currentNote}
+                    setCurrentNote={setCurrentNote}
+                    notes={notes}
+                    setModalShow={setModalShow}
+                    setNotes={setNotes}
+                    setActiveNote={setActiveNote}
+                />
+                <CurrentEditor
+                    notes={notes}
+                    currentNote={currentNote}
+                    setNotes={setNotes}
+                    activeNote={activeNote}
+                    setActiveNote={setActiveNote}
+                />
+            </div>
+
+            <CreateNote
+                modalShow={modalShow}
                 setModalShow={setModalShow}
+                setNotes={setNotes}
             />
-            <CreateNote modalShow={modalShow} setModalShow={setModalShow} />
-        </div>
+        </>
     );
 }
 
