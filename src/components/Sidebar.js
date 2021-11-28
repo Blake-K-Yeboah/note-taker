c51ff4c2eb1b99d867c367f1f24b36cfe2a6e68e
Blake Yeboah <blakekyeboah@gmail.com>
2021-11-28 21:56:46 +1000

    Added NoteLinks

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,30 @@
 import React from "react";
+import NoteLink from "./NoteLink";
 
-const Sidebar = ({ setModalShow }) => {
+const Sidebar = ({
+    notes,
+    currentNote,
+    setCurrentNote,
+    setModalShow,
+    setNotes,
+    setActiveNote,
+}) => {
     return (
-        <div className="bg-gray-200 w-64 h-screen fixed left-0 top-0 z-10">
-            <h2 className="pl-5 text-gray-600 text-2xl py-10">Your Notes</h2>
+        <div className="bg-gray-700 w-64 h-screen fixed left-0 top-0 z-10">
+            <h2 className="pl-5 text-gray-200 text-2xl py-10">Your Notes</h2>
+            {notes.map((note) => {
+                return (
+                    <NoteLink
+                        title={note.title}
+                        active={note.title === currentNote}
+                        setCurrentNote={setCurrentNote}
+                        setNotes={setNotes}
+                        notes={notes}
+                        key={note.title}
+                        setActiveNote={setActiveNote}
+                    />
+                );
+            })}
             <button
                 onClick={() => setModalShow(true)}
                 className="bg-green-500 hover:bg-green-700 transition-all duration-300 ease-in-out text-white px-5 py-3 rounded-xl ml-5 fixed bottom-10"
